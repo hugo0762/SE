a88fb55d41598077e1b6bd1a8446a21697a25f86
hugo0762 <159121258+hugo0762@users.noreply.github.com>
2024-03-22 08:50:13 +0900

    Update qcow2_memoryavailcheck.sh

--- Shell Script/qcow2_memoryavailcheck.sh
+++ Shell Script/qcow2_memoryavailcheck.sh
@@ -1,3 +1,5 @@
+# kvm 서버에 등록된 vm 별 available 메모리 확인
+
 for vm in $(virsh list --all --name ); do
     echo "$vm"  $(virsh dommemstat $vm | grep available)
     #echo $(virsh dommemstat $vm | grep actual)
